8e605ffb49ced992ee3ffbdb57fc9656f2137af7
sonibrothers18 <soni18august2004@gmail.com>
2024-09-23 23:01:44 +0530

    DSA

---
+++ 092_subsequence_of_string_RE_3.cpp
@@ -0,0 +1,33 @@
+//! Important
+//todo In output there is empty string also
+
+#include<iostream>
+#include<vector>
+#include<string.h>
+using namespace std;
+
+void printSubsequence(string str, string output, int index){
+    if(index >= str.length()){
+        cout<<output<<endl;
+        return;
+    }
+
+    //! exclude
+    printSubsequence(str, output, index+1);
+
+    //! include
+    output.push_back(str[index]);
+    printSubsequence(str, output, index+1);
+}
+
+int main(){
+
+    string str = "abc";
+    string output = "";
+
+    int index = 0;
+
+    printSubsequence(str, output, index);
+
+    return 0;
+}
