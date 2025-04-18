ec23602d9778658d1d29727d3ac7d56f5d9c83bf
sonibrothers18 <soni18august2004@gmail.com>
2025-04-19 01:27:16 +0530

    DSA

--- 175_01_topView.cpp
+++ 175_01_topView.cpp
@@ -1,5 +1,8 @@
+//! Top View in Tree
+
 #include<iostream>
 #include<vector>
+#include<map>
 #include<string.h>
 #include<queue>
 #include<stack>
@@ -28,7 +31,9 @@ TreeNode* buildTree(){
 
     TreeNode* root = new TreeNode(data);
 
+    cout<<"Enter left node for "<<data<<" node: ";
     root->left = buildTree();
+    cout<<"Enter right node for "<<data<<" node: ";
     root->right = buildTree();
 
     return root;
@@ -36,13 +41,46 @@ TreeNode* buildTree(){
 
 void top_view_tree(TreeNode* root){
 
-    
+    if(root == NULL)
+        return;
 
+    //! map for storing HD & node data
+    map<int, int> topNode;
+
+    //! store pair consisting of node & HD
+    queue< pair<TreeNode*, int> > q;
+    q.push(make_pair(root, 0));
+
+    while(!q.empty()){
+
+        pair<TreeNode* , int> temp = q.front();
+        q.pop();
+
+        TreeNode* frontNode = temp.first;
+        int HD = temp.second;
+
+        //! check if HD is already present in map or not
+        if(topNode.find(HD) == topNode.end())
+            topNode[HD] = frontNode->data;
+
+        if(frontNode->left)
+            q.push(make_pair(frontNode->left, HD-1));
+
+        if(frontNode->right)
+            q.push(make_pair(frontNode->right, HD+1));
+    }
+
+    for(auto i: topNode){
+        cout<<i.first<<" -> "<<i.second<<endl;
+    }
 }
 
 int main(){
 
-    
+    TreeNode* root = NULL;
+    root = buildTree();
+
+    top_view_tree(root);    
 
     return 0;
 }
