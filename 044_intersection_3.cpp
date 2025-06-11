30cf40060a4cc6db96bdc86b63f65f0a594a0000
sonibrothers18 <soni18august2004@gmail.com>
2025-06-11 18:29:47 +0530

    DSA

--- 044_intersection_3.cpp
+++ 044_intersection_3.cpp
@@ -2,12 +2,26 @@
 
 #include<iostream>
 #include<vector>
+#include <unordered_set>
 using namespace std;
 
 void printVector(vector <int> &ans){
     for(int i = 0; i < ans.size(); i++){
         cout<<ans[i]<<" ";
     }
+}
+
+//! Used for unordered array/vector
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    unordered_set<int> set1(nums1.begin(), nums1.end());
+    unordered_set<int> ans;
+
+    for(auto i: nums2){
+        if(set1.count(i))
+            ans.insert(i);
+    }
+
+    return vector<int>(ans.begin(), ans.end());
 }
 
 int main(){
